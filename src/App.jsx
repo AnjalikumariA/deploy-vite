b7b297c09ca99b2290c6c08e61157eeba5210f42
AnjalikumariA <a4alina.anjali@gmail.com>
2026-05-29 11:45:50 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+      
       <h1>hello world</h1>
       <h2>this is our how to deploy a React app on github pages </h2>
       <h1>thank you</h1>
       <h1>best regards</h1>
-      <h1></h1>
+      <h1> Remember who you wanted to be</h1>
+      <p>No more expectations</p>
+      <p>Just be Yourself</p>
+
     </div>
   )
 }
